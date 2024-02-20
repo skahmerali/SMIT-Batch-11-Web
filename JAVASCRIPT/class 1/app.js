c0d6554184c14fb28ab3aed7e58a60646dd478e4
Shehzad Iqbal <>
2024-02-20 22:56:40 +0500

    start javascript
    start javascript

---
+++ JAVASCRIPT/class 1/app.js
@@ -0,0 +1,14 @@
+// var firstName = "Sheikh Ahmer";
+// var lastName = "Ali";
+// alert("Hi !" + firstName + " " +lastName);
+var classStudents = 30;
+var totelAplyedStudents = 1000;
+alert( classStudents + totelAplyedStudents);
+
+// var firstName = Number(prompt("Enter you first name:"));
+// var lastName = Number(prompt("Enter you last name:"));
+var firstName = prompt("Enter you first name:");
+// var firstName = "";
+var lastName = prompt("Enter you last name:");
+
+alert("you desired resutlt is : " + (firstName +lastName));
