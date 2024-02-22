135d23626afca4c5416b48b3be9fde5282bf6e88
Shehzad Iqbal <38328235+skahmerali@users.noreply.github.com>
2024-02-22 22:54:34 +0500

    if else , nested and operators
    logical description

---
+++ JAVASCRIPT/class 2/app.js
@@ -0,0 +1,49 @@
+// var firstName = 'ahmer';
+// alert(FIRSTNAME);
+
+// var resultPercent = ((50 / 100) * 100) ;
+// console.log(resultPercent);
+
+// var intialNum = 1;
+// intialNum = intialNum++ ;
+// console.log(intialNum);
+
+// intialNum = intialNum++
+// intialNum = ++intialNum
+// console.log(intialNum)
+
+// intialNum = intialNum + 1
+// console.log(intialNum);
+
+
+// var numValue = (((1 / 2) + (3 * 4)) - 8 + 6);
+// console.log(numValue)
+var userName = prompt("Enter ur email ")
+var userPass = prompt("Enter ur password ")
+// var userName = 1
+// if (userName === 'Ahmer Ali') {
+//     var userPass = prompt("Enter ur password ")
+//     if (userPass === '123') {
+//         alert("congratulation, u r a user")
+//     } else {
+//         alert("sorry ur password is wrong!")
+
+//     }
+// } else {
+// alert("sorry u r not valid!")
+// alert("sorry ur email is wrong")
+// }
+// if (userName === 'sheikhahmerali75@gmail.com' && (userPass === "123"
+//     || userPass === "321")) {
+//     alert("succefully login !");
+// } else {
+//     alert("email or password is incorrect");
+// }
+
+if (userName === 'sheikhahmerali75@gmail.com' && userPass === "123") {
+    alert("succefully login !");
+} else if (userName === 'sheikhahmerali75@gmail.com' && userPass === "321") {
+    alert("succefully login !");
+}else{
+    alert("email or password is incorrect");
+}
