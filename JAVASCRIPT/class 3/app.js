6b5d33faaed5a89ee21c17fdb24f89b56058d0fb
alihuzaifa <38328235+skahmerali@users.noreply.github.com>
2024-02-24 23:00:10 +0500

    arrays and its methods
    arrays and its methods

---
+++ JAVASCRIPT/class 3/app.js
@@ -0,0 +1,47 @@
+// var a = 1;
+// var b = 2;
+
+// a = ++b; //2 b=3
+// console.log(b);
+// console.log(a);
+
+// console.log(b);
+// b = a++; 
+// console.log(b);
+// b = ++a;
+// console.log(a);
+// a = ++a;
+// console.log(a);
+// a = a++;
+// console.log(a);
+// a = ++a
+// console.log(a);
+
+// console.log(a)
+// console.log(b)
+
+
+// var a = 2;
+// a++ ;
+// if (a > 2) {
+//     console.log(a);
+// } else if (a < 2) {
+//     console.log("happy world");
+
+// } else {
+//     console.log("Pakistan zindabad");
+// }
+var hasnain = "hasnain"
+
+var classStudents = ["hussain", "kaif", "ismail", "zain", hasnain]
+// console.log(classStudents);
+// var newStd = prompt("Enter your name!")
+// classStudents.push(newStd)
+// console.log(classStudents);
+// console.log(classStudents.pop())
+// classStudents.pop()
+// console.log(classStudents);
+// console.log(classStudents[2]);
+// classStudents.splice(1 , 3)
+console.log(classStudents.splice(1 , 1, 'ahmer', 'ali'));
+console.log(classStudents);
---
+++ JAVASCRIPT/class 3/text.md
@@ -0,0 +1,12 @@
+*** Important ***
+1 : kaif a = 1 b= 2
+2 : asad : a = 2 b = 2
+3: hasan : a= 2 b =2
+4: moazzam : a = 2 b=3
+5 : jawed : a 1  b =1
+6 : hussain : a=2 b=1
+7 hamza : a = 3 b =3 
+8 Mhamza : a=3 b =2 , b= 3 a=2;
+9 jawad : a=3 b=3
+
+
